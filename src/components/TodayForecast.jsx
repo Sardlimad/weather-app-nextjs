e22a5176643c21efa9a398e42ac2ea9d9cfbe2d9
David Sardlimad <genesis.david.08@gmail.com>
2024-12-11 13:27:14 -0500

    Skeleton separated

--- src/components/TodayForecast.jsx
+++ src/components/TodayForecast.jsx
@@ -9,15 +9,19 @@ export const TodayForecast = ({ hour, loading }) => {
 
 
     const items = hour.map((item) => {
-        return <Skeleton isLoaded={!loading} key={item.time}><TodayItem time={formatTo12Hour(item.time)} temp={item.temp_c} img={getIcon(item.code, item.is_day)} /></Skeleton>
+        return <TodayItem time={formatTo12Hour(item.time)} temp={item.temp_c} img={getIcon(item.code, item.is_day)} />;
     });
+
+    const skel = () => {
+        return <Skeleton><Text>Hola</Text></Skeleton>
+    }
 
 
     return (
         <Box bgColor="gray.100" borderRadius="15px" p={3} pt="5px">
             <Heading fontSize="sm" fontWeight={200} m="10px" >TODAY'S FORECAST</Heading>
             <HStack divider={<StackDivider borderColor='gray.500' />} >
-                {items}
+                {!loading ? items : skel}
             </HStack>
         </Box>
     )
