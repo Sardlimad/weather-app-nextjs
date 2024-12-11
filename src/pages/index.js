f7b12f142936c4e1eb77dab719fff95ffaa3a836
David Sardlimad <genesis.david.08@gmail.com>
2024-12-11 14:36:15 -0500

    empty astro object adden for connection problems

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,13 @@ import { Astro } from "@/components/Astro";
 export default function Home() {
   const [query, setQuery] = useState("530075");
   const [searchValue, setSearchValue] = useState("");
-  const [data, setData] = useState({ main: {}, air: {}, hour: [], days: [] });
+  const [data, setData] = useState({
+    main: {},
+    astro: {},
+    air: {},
+    hour: [],
+    days: [],
+  });
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -23,7 +29,13 @@ export default function Home() {
           setData(fetchedData);
         } else {
           console.error("No data received");
-          setData({ main: {}, air: {}, hour: [], days: [] });
+          setData({
+            main: {},
+            astro: {},
+            air: {},
+            hour: [],
+            days: [],
+          });
         }
       })
       .catch((error) => console.error("Error fetching data:", error))
