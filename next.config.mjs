078fba8b8b0d407970b4dc38d37338ecf4ebfb48
David Sardlimad <genesis.david.08@gmail.com>
2024-12-11 20:42:33 -0500

    Updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = {
-  trailingSlash: true,
-  assetPrefix: "./",
-  basePath: "",
-  images: {
-    unoptimized: true,
-  },
   output: "export",
 };
 
