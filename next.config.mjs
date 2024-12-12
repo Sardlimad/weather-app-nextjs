0d93e4ef0a31aaedfe37b302dff7598430c6645a
David Sardiñas Lima <genesis.david.08@gmail.com>
2024-12-11 20:40:04 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = {
-  trailingSlash: true,
-  assetPrefix: "./",
-  basePath: "",
-  images: {
-    unoptimized: true,
-  },
-  output: "export",
-};
-
 export default nextConfig;
